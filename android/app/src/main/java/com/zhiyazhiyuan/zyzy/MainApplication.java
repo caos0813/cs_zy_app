1b712392adcdfbe5c79cc6befafd57972bb20737
ainuo <448401921@qq.com>
2018-11-09 18:23:04 +0800

    no message

--- android/app/src/main/java/com/zhiyazhiyuan/zyzy/MainApplication.java
+++ android/app/src/main/java/com/zhiyazhiyuan/zyzy/MainApplication.java
@@ -37,7 +37,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG,"http://39.104.165.143:3000/"),
             new WeChatPackage(),
             new SvgPackage(),
